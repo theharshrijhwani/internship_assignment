00cfedda266a98f5675021e8f56c123efbef19a3
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2023-11-01 17:15:22 +0530

    a lil bit css changes

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,36 +1,19 @@
 import React from "react";
 
 import "./pcard.css";
-import { demo } from "../../data/productdata";
-import { product_data } from "../../data/productdata";
 
-const ProductCard = () => {
+const ProductCard = ({ image, title, category, desc, price }) => {
   return (
-    <div className="card-container">
-      {product_data.map((item, idx) => (
-        <div className="card" key={idx}>
-          <div className="image">
-            <img width={200} height={200} src={item.image} />
-          </div>
-          <div className="title">{item.name}</div>
-          <div className="category">{item.category}</div>
-          <div className="desc">{item.desc}</div>
-          <div className="price">${item.price}</div>
-        </div>
-      ))}
-      {/* <div className="card">
-        <div className="image">
-          <img width={200} height={200} src={demo.image} />
-        </div>
-        <div className="title">{demo.name}</div>
-        <div className="category">{demo.category}</div>
-        <div className="desc">{demo.desc}</div>
-        <div className="price">{demo.price}</div>
-      </div> */}
+    <div className="card">
+      <div className="image">
+        <img width={200} height={200} src={image} />
+      </div>
+      <div className="title">{title}</div>
+      <div className="category">{category}</div>
+      <div className="desc">{desc}</div>
+      <div className="price">${price}</div>
     </div>
   );
 };
 
 export default ProductCard;
-
-// { image, title, category, desc, price }
--- src/components/ProductWindow/ProductWindow.jsx
+++ src/components/ProductWindow/ProductWindow.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import "./pwindow.css";
 import ProductCard from "../ProductCard/ProductCard";
 
+import { product_data } from "../../data/productdata";
+
 const ProductWindow = () => {
   return (
     <div className="container">
@@ -10,8 +12,20 @@ const ProductWindow = () => {
         <div className="heading">
           <h1>Products Available</h1>
         </div>
+        <div className="options"></div>
         <div className="card-section">
-          <ProductCard />
+          <div className="card-container">
+            {product_data.map((item, idx) => (
+              <ProductCard
+                key={idx}
+                title={item.name}
+                price={item.price}
+                image={item.image}
+                desc={item.desc}
+                category={item.category}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
